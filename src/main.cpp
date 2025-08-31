29c001dc2a14e91edf5f02a671fe99bfeda34a6b
IgorJoaquimn <igor-joaquim@ufmg.br>
2025-08-31 15:35:28 -0300

    lets win

--- src/main.cpp
+++ src/main.cpp
@@ -1,70 +1,7 @@
-#include <SDL.h>
-#include <GL/glew.h>
-#include <SDL_opengl.h>
-#include <iostream>
+#include "Game.hpp"
 
 int main(int argc, char* argv[]) {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
-        return 1;
-    }
-
-    // Set OpenGL attributes
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-
-    SDL_Window* window = SDL_CreateWindow("Infinite Craft Clone",
-                                          SDL_WINDOWPOS_UNDEFINED,
-                                          SDL_WINDOWPOS_UNDEFINED,
-                                          800, 600,
-                                          SDL_WINDOW_OPENGL);
-
-    if (!window) {
-        std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
-        SDL_Quit();
-        return 1;
-    }
-
-    SDL_GLContext glContext = SDL_GL_CreateContext(window);
-    if (!glContext) {
-        std::cerr << "OpenGL context could not be created! SDL_Error: " << SDL_GetError() << std::endl;
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 1;
-    }
-
-    // This must be set before calling glewInit()
-    glewExperimental = GL_TRUE;
-    // Initialize GLEW
-    GLenum glewStatus = glewInit();
-    if (glewStatus != GLEW_OK) {
-        std::cerr << "GLEW initialization failed: " << glewGetErrorString(glewStatus) << std::endl;
-        SDL_GL_DeleteContext(glContext);
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 1;
-    }
-
-    bool running = true;
-    SDL_Event event;
-
-    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
-    while (running) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                running = false;
-            }
-        }
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        SDL_GL_SwapWindow(window);
-    }
-
-    SDL_GL_DeleteContext(glContext);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    Game game;
+    game.run();
     return 0;
 }
-
